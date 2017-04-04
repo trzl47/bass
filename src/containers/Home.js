2b49e2dac98052919df12e28b693d11b18dfb992
trzl47 <brownty47@gmail.com>
2017-04-03 21:28:04 -0500

    adding final components

---
+++ src/containers/Home.js
@@ -0,0 +1,18 @@
+// Libs
+import React, { Component } from 'react';
+// Components
+import Banner from './Banner.js';
+import AboutSupporting from './About.js';
+import Download from './Download.js';
+
+export default class Home extends Component {
+	render() {
+		return(
+			<div className='bass'>
+				<Banner />
+				<AboutSupporting />
+				<Download />
+			</div>
+		);
+	}
+}
