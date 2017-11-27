20ee9f614f4ec3dae34f58cf7973ca6a19293292
trzl47 <brownty47@gmail.com>
2017-11-27 13:45:58 -0600

    updated project to docker dev process

---
+++ services/web/src/app/containers/Download.js
@@ -0,0 +1,18 @@
+// Libs
+import React, { Component } from 'react';
+// Components
+
+export default class Download extends Component {
+	render() {
+		return (
+			<div className='download'>
+				<div className='container'>
+					<h2>Try it now</h2>
+					<a href="bass.tbrownkcdev.com"><img alt='' role='presentation' src={require('../../static/img/app-store.png')} /></a>
+					<a href="bass.tbrownkcdev.com"><img alt='' role='presentation' src={require('../../static/img/google-play.png')} /></a>
+				</div>
+			</div>
+		);
+	}
+
+}
