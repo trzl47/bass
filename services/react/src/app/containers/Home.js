ffd07f0e332c91cfaeb24e8b15482cf3c394b231
trzl <brownty47@gmail.com>
2018-06-25 15:00:03 -0500

    Adding in About component
    uncommented component in Home container and created stylesheets for infobars

--- services/react/src/app/containers/Home.js
+++ services/react/src/app/containers/Home.js
@@ -10,7 +10,7 @@ export default class Home extends Component {
 		return(
 			<div className='bass'>
 				<Banner />
-				{/* <AboutSupporting /> */}
+				<AboutSupporting />
 				{/* <Download /> */}
 			</div>
 		);
